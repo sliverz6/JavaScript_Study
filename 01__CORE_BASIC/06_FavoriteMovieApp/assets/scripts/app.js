599253d29499dc9ce6731de11ac43f15d077f6c8
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-04-10 13:15:43 +0900

    DOM 연습

---
+++ 01__CORE_BASIC/06_FavoriteMovieApp/assets/scripts/app.js
@@ -0,0 +1,29 @@
+const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.body.children[1];
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild;
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => { // or function() {}
+    addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+    toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
