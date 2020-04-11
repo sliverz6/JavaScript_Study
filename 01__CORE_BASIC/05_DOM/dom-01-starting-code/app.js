d8f3f3188dbaf686150e50bf692795d71cf0b3ea
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-04-11 21:55:50 +0900

    DOM 공부중

---
+++ 01__CORE_BASIC/05_DOM/dom-01-starting-code/app.js
@@ -0,0 +1,15 @@
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+// section.style.backgroundColor = 'blue';
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+    // if (section.className === 'red-bg visible') {
+    //     section.className = 'red-bg invisible';
+    // } else {
+    //     section.className = 'red-bg visible';
+    // }
+    // section.classList.toggle('visible');
+    section.classList.toggle('invisible');
+});
