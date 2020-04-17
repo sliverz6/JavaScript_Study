98608a3476ccbe35dfd1eab38f4528f24fbf9e9a
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-04-17 20:30:48 +0900

    Update app2.js

--- 01__CORE_BASIC/07_Advanced_Array/app2.js
+++ 01__CORE_BASIC/07_Advanced_Array/app2.js
@@ -55,14 +55,109 @@
 // Selecting Range & Creating Copies
 /********************************************/
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
-// const storedResults = testResults.slice(2);
-const storedResults = testResults.concat([3.99, 2]);
+// const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// // const storedResults = testResults.slice(2);
+// const storedResults = testResults.concat([3.99, 2]);
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
-console.log(testResults.lastIndexOf(1.5));
+// console.log(storedResults, testResults);
+// console.log(testResults.lastIndexOf(1.5));
 
-const personData = [{name: 'Max'}, {name: 'Manuel'}];
-console.log(personData.indexOf({name: 'Manuel'}));
+// console.log(testResults.includes(10.99));
+
+// const personData = [{name: 'Max'}, {name: 'Manuel'}];
+// console.log(personData.indexOf({name: 'Manuel'}));
+
+// const manuel = personData.find((person, idx, persons) => {
+//    return person.name === 'Manuel'; 
+// });
+
+// manuel.name = 'Anna';
+
+// console.log(manuel, personData);
+
+// const maxIndex = personData.findIndex((person, idx, persons) => {
+//     return person.name === 'Max'; 
+//  });
+
+//  console.log(maxIndex);
+
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
+
+// // for (const price of prices) {
+// //     taxAdjustedPrices.push(price * (1 + tax));
+// // }
+
+// prices.forEach((price, idx, prices) => {
+//     const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
+//     taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
+    const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
+    return priceObj;
+});
+
+// console.log(prices, taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+    if (a > b) {
+        return 1;
+    } else if (a === b) {
+        return 0;
+    } else {
+        return -1;
+    }
+});
+console.log(sortedPrices);
+
+const filteredArray = prices.filter(p => p > 6);
+
+console.log(filteredArray);
+
+// let sum = 0;
+
+// prices.forEach((price) => {
+//     sum += price;
+// });
+
+// console.log(sum);
+
+const sum = prices.reduce((prevValue, curValue, curIndex, prices) => prevValue + curValue, 0);
+
+console.log(sum);
+
+const data = 'new york;10.99;2000';
+
+const transformedData = data.split(';');
+transformedData[1] = +transformedData[1];
+console.log(transformedData);
+
+const nameFragments = ['Max', 'Schwarz'];
+const name = nameFragments.join(' ');
+console.log(name);
+
+const copiedNameFragments = [...nameFragments];
+nameFragments.push('Mr');
+console.log(nameFragments, copiedNameFragments);
+
+console.log(Math.min(...prices));
+
+const persons = [{name: 'Max', age: 30 }, {name: 'Manuel', age: 31}];
+const copiedPersons = persons.map(person => ({ 
+    name: person.name, 
+    age: person.age
+}));
+
+persons.push({name: 'Anna', age: 29});
+persons[0].age = 31;
+
+console.log(persons, copiedPersons);
