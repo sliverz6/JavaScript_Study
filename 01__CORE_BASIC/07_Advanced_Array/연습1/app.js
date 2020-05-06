a39de9dd2a21b68cf623efce080274e57c3faf78
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-05-06 12:12:27 +0900

    배열 공부중

--- 01__CORE_BASIC/07_Advanced_Array/연습1/app.js
+++ 01__CORE_BASIC/07_Advanced_Array/연습1/app.js
@@ -1,14 +1,38 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const moreNumbers = new Array(5, 2); // []
-// console.log(moreNumbers);
+// // const moreNumbers = new Array(5, 2); // []
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2, 3);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2, 3);
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
+
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', {moreDetail: []}];
+
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+// for (const data of analyticsData) {
+//     for (const dataPoint of data) {
+//         console.log(dataPoint);
+//     }
+// }
+
+// console.log(personalData[1]);
+
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+hobbies[5] = 'Reading'; // rarely used
+console.log(hobbies);
